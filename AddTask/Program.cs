77763bec17962e05ee1e0a25c31aed8cd8ebf7e2
JuliaTrushkova <kosyuska@yandex.ru>
2022-02-05 23:04:15 +0300

    feat: add AddTask

---
+++ AddTask/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+delegate double MyDelegate(int argument1, int argument2, int argument3);
+
+class Program
+{
+    public static int Method()
+    {
+        Random random = new Random();
+        int ran = random.Next(0, 100);
+        Console.WriteLine(ran);
+        return ran;
+    }
+
+    static void Main()
+    { 
+        MyDelegate myDelegate1 = delegate (int a, int b, int c)
+        {
+            return (a + b + c)/3;
+        };
+
+        double result = myDelegate1(Method(), Method(), Method());
+        Console.WriteLine("Среднеарифметическое равно {0}", result);
+    }
+}
+
